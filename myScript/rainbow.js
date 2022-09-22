a059914162a47c7af6fb28cab3f0a8d2b60d4be6
DevKyle29 <kylebrentfry@gmail.com>
2022-09-22 11:42:45 +0200

    Completed
    Interactive Rainbow "Click for change"

---
+++ myScript/rainbow.js
@@ -0,0 +1,58 @@
+//RAINBOW CHALLENGE
+
+//STEP 1 : Create the function
+//STEP 2 : Create the variable inside the the function that will target the ID tags
+//STEP 3 : variableName.style.backgroundcolor = "the color you want the background to be";
+
+
+function redStyle(){
+    var element = document.getElementById("R");
+    element.style.backgroundColor = "red";
+    
+    
+}
+
+function orangeStyle(){
+    var element = document.getElementById("O");
+    element.style.backgroundColor = "orange";
+    
+    
+}
+
+function yellowStyle(){
+    var element = document.getElementById("Y");
+    element.style.backgroundColor = "yellow";
+    
+    
+}
+
+function greenStyle(){
+    var element = document.getElementById("G");
+    element.style.backgroundColor = "green";
+    
+    
+}
+function blueStyle(){
+    var element = document.getElementById("B");
+    element.style.backgroundColor = "blue";
+    
+    
+}
+
+function DarkPurple(){
+    var element = document.getElementById("Dp");
+    element.style.backgroundColor = "#56144D";
+    
+    
+}
+
+function torStyle(){
+    var element = document.getElementById("T");
+    element.style.backgroundColor = "turquoise";
+    
+    
+}
+
+
+    
+
